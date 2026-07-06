3ae948049b730031119148c3d1785fe961a89ade
khuzema24602 <khuzemaakhtar056@gmail.com>
2026-07-06 11:46:41 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversion.js
@@ -0,0 +1,22 @@
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof (score));
+
+let vlueinnumbr = Number(score)
+console.log(typeof vlueinnumbr);
+console.log(vlueinnumbr);
+// "33"=> 33
+// "33abc"=> NaN
+//true=>1;false=>0
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+//1=>true;0=>false
+//""=>false
+//"khuzema"=>true
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
